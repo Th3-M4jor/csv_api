f26370489e18dde567c282ea6059434b64b61609
The Major <spartan364@gmail.com>
2022-02-09 21:35:33 -0800

    chore: add dialyxir,credo as dev deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule CsvApi.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
