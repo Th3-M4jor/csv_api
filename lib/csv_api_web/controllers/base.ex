7a0d7164812046b4ee9c2efcb5ab025ab04104e4
The Major <spartan364@gmail.com>
2022-02-10 11:08:59 -0800

    add code documentation

--- lib/csv_api_web/controllers/base.ex
+++ lib/csv_api_web/controllers/base.ex
@@ -1,37 +1,63 @@
 defmodule CsvApiWeb.BaseController do
+  @moduledoc """
+  Using a Single Controller for all the API calls for the sake of simplicity here.
+
+  In a proper application, you would probably want to have a separate controller for each endpoint.
+  """
+
   use CsvApiWeb, :controller
   require Logger
 
-  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
-  def index(conn, req) do
-    render(conn, "index.json", %{data: req})
-  end
+  @doc """
+  Function for replacing all data in the database with the data from the CSV file.
 
+  Phoenix stores uploaded files in a temporary directory, and deletes them when the request is completed.
+  Supports the optional query parameter `?header=false` to indicate that the first line of the CSV file is not a header.
 
-  def put_csv(conn, %{"file" => %Plug.Upload{} = file} = params) do
-    data = File.read!(file.path)
+  This could be made more efficient by using a file Stream, chunking etc, however that would be more complex code.
+  """
+  def put_csv(conn, %{"file" => %Plug.Upload{path: path}} = params) do
+    data = File.read!(path)
 
-    lines = if params["header"] == "false" do
-      String.split(data, ~r/\R/, trim: true)
-    else
-      #has a header, remove it
-      [_header | lines] = String.split(data, ~r/\R/, trim: true)
-      lines
-    end
+    lines =
+      if params["header"] == "false" do
+        # The `\R` regexp greedily matches all linebreak formats.
+        String.split(data, ~r/\R/, trim: true)
+      else
+        # has a header, remove it
+        [_header | lines] = String.split(data, ~r/\R/, trim: true)
+        lines
+      end
 
     ct = CsvApi.Schema.replace_csv(lines)
 
     resp(conn, 200, "#{ct} lines inserted")
-
   rescue
     e ->
       Logger.error(Exception.format(:error, e, __STACKTRACE__))
-      resp(conn, 500, "Error")
+      resp(conn, 500, "Internal Server Error")
   end
 
-  def get_json(conn, _params) do
+  @doc """
+  Get a single order by ID.
+  """
+  def get_order(conn, %{"id" => id}) do
+    id = String.to_integer(id)
+
+    case CsvApi.Schema.get_order_by_id(id) do
+      {:ok, order} ->
+        render(conn, "order.json", %{data: order})
+
+      {:error, reason} ->
+        resp(conn, 404, reason)
+    end
+  end
+
+  @doc """
+  Get all orders.
+  """
+  def get_orders(conn, _params) do
     data = CsvApi.Schema.get_all_orders()
     render(conn, "orders.json", %{data: data})
   end
-
 end
